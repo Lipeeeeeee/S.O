39254583c422b772233f9c2a3b78aa203ff99143
Lipeeeeeee <favasconcelos09@gmail.com>
2024-03-28 12:32:40 -0300

    mv.cpp

---
+++ mv.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <dirent.h>
+
+using namespace std;
+
+int main(int arg, char** args){
+    if(args[3] == nullptr && opendir(args[2]) == NULL) rename(args[1], args[2]);
+    else{
+        cout << get_current_dir_name() << endl;
+        int olddir = open(get_current_dir_name(), O_RDONLY);
+        chdir(args[arg - 1]);
+        cout << get_current_dir_name() << endl;
+        int newdir = open(get_current_dir_name(), O_RDONLY);
+        cout << olddir << ' ' << newdir << endl;
+        for(int i = 1; i < arg - 1; ++i) cout << renameat(olddir, args[i], newdir, args[i]) << endl;
+    }
+}
