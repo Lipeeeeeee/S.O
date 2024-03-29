0728c27b9c2ae065e758485c2ecf60700316ea9c
Lipe Alves <134632259+Lipeeeeeee@users.noreply.github.com>
2024-03-29 13:32:27 -0300

    Update mv.cpp

--- mv.cpp
+++ mv.cpp
@@ -1,20 +1,16 @@
-#include <iostream>
-#include <fcntl.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <dirent.h>
+#include <fcntl.h> // uso da flag 'O_RDONLY'
+#include <stdio.h> // uso do método 'rename e renameat'
+#include <unistd.h> // método para abrir arquivos
+#include <dirent.h> // usado para teste se tem um diretório como argumento ou não
 
 using namespace std;
 
 int main(int arg, char** args){
-    if(args[3] == nullptr && opendir(args[2]) == NULL) rename(args[1], args[2]);
+    if(args[3] == nullptr && opendir(args[2]) == NULL) rename(args[1], args[2]); // se houver 2 argumentos e nenhum for um diretório, mude o nome do arquivo para o nome especificado
     else{
-        cout << get_current_dir_name() << endl;
-        int olddir = open(get_current_dir_name(), O_RDONLY);
-        chdir(args[arg - 1]);
-        cout << get_current_dir_name() << endl;
-        int newdir = open(get_current_dir_name(), O_RDONLY);
-        cout << olddir << ' ' << newdir << endl;
-        for(int i = 1; i < arg - 1; ++i) cout << renameat(olddir, args[i], newdir, args[i]) << endl;
+        int olddir = open(get_current_dir_name(), O_RDONLY); // abre o atual diretório, pegando seu 'fd'
+        chdir(args[arg - 1]); // muda o diretório atual para o que receberá os arquivos
+        int newdir = open(get_current_dir_name(), O_RDONLY); // abre o diretório que receberá os arquivos
+        for(int i = 1; i < arg - 1; ++i) renameat(olddir, args[i], newdir, args[i]); // para cada arquivo especificado como parâmetro, mude-o de lugar para o diretório especificado com o mesmo nome
     }
 }
