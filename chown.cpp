004c46b9deb30e50a04e02983d1f1af9c26d5d45
Lipe Alves <134632259+Lipeeeeeee@users.noreply.github.com>
2024-03-29 13:19:02 -0300

    Update chown.cpp

--- chown.cpp
+++ chown.cpp
@@ -1,16 +1,14 @@
-#include <unistd.h>
-#include <sys/types.h>
-#include <pwd.h>
-#include <grp.h>
-#include <iostream>
-
+#include <unistd.h> // uso do método 'chown'
+#include <sys/types.h> // definição de tipos representando usuários e grupos
+#include <pwd.h> // recuperação de dados do usuário
+#include <grp.h> // recuperação de dados do grupo
 
 using namespace std;
 
 int main(int arg, char** args){
-    struct passwd *user = getpwnam(args[1]);
-    struct group *group = getgrnam(args[1]);
-    uid_t uid = user->pw_uid;
-    gid_t gid = group->gr_gid;
-    chown(args[2], uid, gid);
+    struct passwd *user = getpwnam(args[1]); // recuperando usuário pelo parâmetro
+    struct group *group = getgrnam(args[1]); // recuperando grupo pelo parâmetro
+    uid_t uid = user->pw_uid; // recuperando id do usuário
+    gid_t gid = group->gr_gid; // recuperando id do grupo
+    chown(args[2], uid, gid); // alterando proprietários do arquivo especificado
 }
