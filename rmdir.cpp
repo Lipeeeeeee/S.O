2753f02454ce9de1f9ff73ba30399b7e7ebdeb34
Lipe Alves <134632259+Lipeeeeeee@users.noreply.github.com>
2024-03-29 13:35:48 -0300

    Update rmdir.cpp

--- rmdir.cpp
+++ rmdir.cpp
@@ -1,9 +1,10 @@
-#include <unistd.h>
-#include <iostream>
+#include <unistd.h> // uso do método de remoção de diretório
+#include <iostream> // saída de dados
 
 using namespace std;
 
 int main(int arg, char** args){
-    int ans = rmdir(args[1]);
-    if(ans == -1) cout << "O diretório precisa estar vazio para ser excluído\n";
+    int ans = rmdir(args[1]); // variável armazenando o resultado da remoção do diretório
+    if(ans == -1) cout << "O diretório precisa estar vazio para ser excluído\n"; // caso o diretório tenha algum conteúdo, informará na saída de dados
+    // caso contrário, o diretório foi removido
 }
