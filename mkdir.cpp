cebc5aed0f21501a8d613666cf1b49473608de01
Lipe Alves <134632259+Lipeeeeeee@users.noreply.github.com>
2024-03-29 13:27:10 -0300

    Update mkdir.cpp

--- mkdir.cpp
+++ mkdir.cpp
@@ -1,8 +1,8 @@
-#include <sys/stat.h>
-#include <fcntl.h>
+#include <sys/stat.h> // uso do método 'mkdir'
+#include <fcntl.h> // uso da flag 'O_CREAT'
 
 using namespace std;
 
 int main(int arg, char** args){
-    mkdir(args[1], O_CREAT);
+    mkdir(args[1], O_CREAT); // cria um novo diretório com o nome especificado, somente com permissão de criação
 }
