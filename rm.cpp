917d9ee89a0388483c67dc6bd7ce17f88783ab72
Lipe Alves <134632259+Lipeeeeeee@users.noreply.github.com>
2024-03-29 13:34:04 -0300

    Update rm.cpp

--- rm.cpp
+++ rm.cpp
@@ -1,10 +1,10 @@
-#include <fcntl.h>
-#include <unistd.h>
-#include <iostream>
+#include <fcntl.h> // uso do método para abrir arquivo
+#include <unistd.h> // uso do método de remoção do arquivo
+#include <iostream> // saída de dados
 
 using namespace std;
 
 int main(int arg, char** args){
-    if(open(args[1], O_RDONLY) == -1) cout << "Arquivo não existe\n";
-    else unlink(args[1]);
+    if(open(args[1], O_RDONLY) == -1) cout << "Arquivo não existe\n"; // se o arquivo não existe, informe como saída
+    else unlink(args[1]); // caso contrário, desvincule o arquivo, assim o excluindo
 }
