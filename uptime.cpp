2521ccef2c989a223b9505a23e4ea166da9bf007
Lipe Alves <134632259+Lipeeeeeee@users.noreply.github.com>
2024-03-29 12:59:21 -0300

    Update uptime.cpp

--- uptime.cpp
+++ uptime.cpp
@@ -1,14 +1,14 @@
-#include <sys/sysinfo.h>
-#include <iostream>
+#include <sys/sysinfo.h> // recupera informações do sistema
+#include <iostream> // saída de dados
 
 using namespace std;
 
 int main(){
-    struct sysinfo info;
-    sysinfo(&info);
-    int d = info.uptime / 86400;
-    int h = info.uptime % 86400 / 3600;
-    int m = info.uptime % 3600 / 60;
-    int s = info.uptime % 60;
-    cout << d << ':' << h << ':' << m << ':' << s << endl;
+    struct sysinfo info; // struct com os dados do sistema
+    sysinfo(&info); // recuperação de dados na variável 'info'
+    int d = info.uptime / 86400; // info.uptime - retorna em segundos o tempo desde a última inicialização do sistema. Formatação dos dias
+    int h = info.uptime % 86400 / 3600; // formatação das horas
+    int m = info.uptime % 3600 / 60; // formatasção dos minutos
+    int s = info.uptime % 60; // formatação dos segundos
+    cout << d << ':' << h << ':' << m << ':' << s << endl; // saída em dias:horas:minutos:segundos
 }
