39154935dceeff4c4a16ad89ce94486bfab33590
Lipe Alves <134632259+Lipeeeeeee@users.noreply.github.com>
2024-03-29 13:05:08 -0300

    Update cat.cpp

--- cat.cpp
+++ cat.cpp
@@ -1,11 +1,11 @@
-#include <fcntl.h>
-#include <unistd.h>
+#include <fcntl.h> // abrir arquivos
+#include <unistd.h> // ler e escrever em arquivos
 
 using namespace std;
 
 int main(int arg, char** args){
-    int f = open(args[1], O_RDONLY);
-    char buf[1024];
-    int s;
-    while((s = read(f, buf, 1024)) != 0) write(1, buf, s);
+    int f = open(args[1], O_RDONLY); // arquivo aberto para leitura do conteúdo
+    char buf[1024]; // array guardando o conteúdo de 1024 bytes em 1024 bytes
+    int s; // variável com a quantidade de bytes lidos
+    while((s = read(f, buf, 1024)) != 0) write(1, buf, s); // enquanto tiver bytes para serem lidos, escreva no arquivo de saída (1) o que estiver em 'buf', a quantidade de bytes de 's'
 }
