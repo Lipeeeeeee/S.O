3d271040f6c99ac38baa885975b140fb2fff0afb
Lipeeeeeee <favasconcelos09@gmail.com>
2024-07-27 11:59:47 -0300

    inicio do history

--- mv.cpp
+++ mv.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int main(int arg, char** args){
     if(args[3] == nullptr && opendir(args[2]) == NULL) rename(args[1], args[2]);
     else{
-        cout << get_current_dir_name() << endl;
+        
         int olddir = open(get_current_dir_name(), O_RDONLY);
         chdir(args[arg - 1]);
         cout << get_current_dir_name() << endl;
--- shell.cpp
+++ shell.cpp
@@ -1,11 +1,28 @@
 #include <iostream>
 #include <unistd.h>
+#include <vector>
 #include <sys/wait.h>
 
-void process_command(std::string command) {
+
+void process_command(std::string command, std::vector<std::string> history) {
     // Se for comando interno...
-    if (command == "exit")
-        exit(0);
+    if (command == "exit") exit(0);
+    else if (command == "pwd"){
+        history.push_back(command);
+        std::cout << get_current_dir_name() << std::endl;
+    }
+    else if (command.find("history") != -1){
+        if(command.find(" -c") != -1) history.clear();
+        else{
+            int size = history.size();
+            if(size >= 10){
+                for(int i = 9; i > 0; --i) std::cout << i << " " << history[size - i - 1] << std::endl;
+            }
+            else{
+                for(int i = size - 1; i > 0; --i) std::cout << i << " " << history[size - i] << std::endl;
+            }
+        }
+    }
 
     // Se for comando externo
 
@@ -13,36 +30,53 @@ void process_command(std::string command) {
     /*  Se for caminho relativo, procurar o comando na lista de diretórios
         Se for absoluto verifica se comando existe
     */
-    std::string absolute_path = "/mnt/c/Users/20231014040027/OneDrive - IFRN/S.O/" + command; 
-    if (access(absolute_path.c_str(), F_OK) == 0) { // Arquivo existe no diretório
-        if (access(absolute_path.c_str(), X_OK) == 0) { // Arquivo é executável
-            pid_t pid = fork();
-            if (pid < 0){ // Erro
-                std::cout << "Erro de execução!" << std::endl;
-                return;
-            } else if (pid == 0){ //processo filho
-                char * argv[2] = {(char *)command.c_str(), nullptr};
-                execve(absolute_path.c_str(), argv, NULL);
-            } else { // Processo pai
-                /* Deve adicionar processo filho na lista (std::vector) 
-                   de processos em execução para gerenciar background. */
-                /* Processo pai espera processo filho terminar. */
-                waitpid(pid, nullptr, 0); 
+    else{
+        std::string absolute_path = command.find(" ") != -1 ? "/mnt/c/Users/alves/OneDrive - IFRN/S.O/" + command.substr(0, command.find(" ")) : "/mnt/c/Users/alves/OneDrive - IFRN/S.O/" + command;
+        if (access(absolute_path.c_str(), F_OK) == 0) { // Arquivo existe no diretório
+            if (access(absolute_path.c_str(), X_OK) == 0) { // Arquivo é executável
+                pid_t pid = fork();
+                if (pid < 0){ // Erro
+                    std::cout << "Erro de execução!" << std::endl;
+                    return;
+                } else if (pid == 0){ //processo filho
+                    int params = 0;
+                    std::string command_copy = command;
+                    while(command_copy.find(" ") != -1){
+                        ++params;
+                        command_copy = command_copy.substr(command_copy.find(" ") + 1, command_copy.size() - 1);
+                    }
+                    char * argv[params+2] = {(char *)command.c_str()};
+                    history.push_back(command);
+                    if(command.find(" ") != -1){
+                        for(int i = 1; i <= params; ++i){
+                            command = command.substr(command.find(" ") + 1, command.size() - 1);
+                            argv[i] = (char *)command.c_str();
+                        }
+                    }
+                    argv[params + 2 - 1] = nullptr;
+
+                    execve(absolute_path.c_str(), argv, NULL);
+                } else { // Processo pai
+                    /* Deve adicionar processo filho na lista (std::vector) 
+                    de processos em execução para gerenciar background. */
+                    /* Processo pai espera processo filho terminar. */
+                    waitpid(pid, nullptr, 0); 
+                }
             }
-        } else { // Arquivo não é executável
-            std::cout << "permission denied: " << command << std::endl;
-        }
-    } else { // Arquivo não existe
-        std::cout << "Command not found: " << command << std::endl;
+            else std::cout << "permission denied: " << command << std::endl; // Arquivo não é executável
+        } 
+        else std::cout << "Command not found: " << command << std::endl; // Arquivo não existe
     }
 }
 
 int main() {
+    std::vector<std::string> history;
     while (true) {
         std::cout << "$> ";
         std::string command;
         getline(std::cin, command);
-        process_command(command);
+        process_command(command, history);
+        std::cout << history.size() << std::endl;
     }
     return 0;
 }
