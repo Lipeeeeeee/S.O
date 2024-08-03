6e719b9e6ce7087e01e8ec5d3bda58dfcb8ae2f6
Lipeeeeeee <favasconcelos09@gmail.com>
2024-08-03 11:41:30 -0300

    Sem diretório exe

--- mv.cpp
+++ mv.cpp
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-#include <iostream>
-#include <fcntl.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <dirent.h>
-
-using namespace std;
-
-int main(int arg, char** args){
-    if(args[3] == nullptr && opendir(args[2]) == NULL) rename(args[1], args[2]);
-    else{
-        
-        int olddir = open(get_current_dir_name(), O_RDONLY);
-        chdir(args[arg - 1]);
-        cout << get_current_dir_name() << endl;
-        int newdir = open(get_current_dir_name(), O_RDONLY);
-        cout << olddir << ' ' << newdir << endl;
-        for(int i = 1; i < arg - 1; ++i) cout << renameat(olddir, args[i], newdir, args[i]) << endl;
-    }
-}
-=======
 #include <fcntl.h> // uso da flag 'O_RDONLY'
 #include <stdio.h> // uso do método 'rename e renameat'
 #include <unistd.h> // método para abrir arquivos
@@ -36,4 +14,3 @@ int main(int arg, char** args){
         for(int i = 1; i < arg - 1; ++i) renameat(olddir, args[i], newdir, args[i]); // para cada arquivo especificado como parâmetro, mude-o de lugar para o diretório especificado com o mesmo nome
     }
 }
->>>>>>> e4f51742789ae5520e90f8166663228d1a8ba03c
